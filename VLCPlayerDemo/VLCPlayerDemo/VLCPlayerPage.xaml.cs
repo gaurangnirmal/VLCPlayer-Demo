8fed432b61088e919da924f1748f9784687235da
Gaurang.nirmal <gaurang11781@gmail.com>
2020-09-14 22:31:49 +0530

    initial commit

---
+++ VLCPlayerDemo/VLCPlayerDemo/VLCPlayerPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VLCPlayerDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VLCPlayerPage : ContentPage
+    {
+        public VLCPlayerPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnAppearing(object sender, System.EventArgs e)
+        {
+            base.OnAppearing();
+            ((VLCPlayerViewModel)BindingContext).OnAppearing();
+        }
+
+        void OnDisappearing(object sender, System.EventArgs e)
+        {
+            base.OnDisappearing();
+            ((VLCPlayerViewModel)BindingContext).OnDisappearing();
+        }
+    }
+}
